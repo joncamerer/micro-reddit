e8763c04670bf4d4d2cec5398ca7f06b4cd35c90
Jon Camerer <jon.camerer@gmail.com>
2017-09-22 16:22:16 -0500

    Add User model with validations

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  validates :name,     presence: true, length: { maximum: 50 },
+                       uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
+end
