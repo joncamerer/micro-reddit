9d915c2da0da99ccc84f077ba9c86cbd224d7779
Jon Camerer <jon.camerer@gmail.com>
2017-09-25 09:19:15 -0500

    Add posts model and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  has_many :posts
+
   validates :name,     presence: true, length: { maximum: 50 },
                        uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
+
   has_secure_password
 end
